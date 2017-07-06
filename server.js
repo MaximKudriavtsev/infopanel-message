282ced6acc9b60f90a92d2bdf5ea78545b5d24b2
Maxim Kudryavtsev <maxim71ruswow@gmail.com>
2017-07-06 17:23:48 +0300

    added 2 working querys -   /query_users  - return users list from json file -   /send_data     - send object to server side

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
+var bodyParser = require('body-parser');
 
 var tableData = require('./tableData.json');
 
@@ -12,17 +13,27 @@ var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
-app.get("/", function(req, res) {
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/query", function(req, res) {
+app.get('/query_users', function(req, res) {
   res.send(tableData);
 });
 
-app.post("/command", function(req, res) {
-  res.send('=> /query');
-  console.log('=> /query');
+app.post('/send_data', function(req, res, next) {
+  res.send('=> /send_data');
+  console.log('=> /send_data');
+  console.log(req.body);
 });
 
 app.listen(port, function(error) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,44 +1,102 @@
+import $ from 'jquery';
+
 const initialState = {
     text: '',
     author: 'User156',
     location: '',
     eventDate: new Date(),
     startDate: new Date(),
-    messageAuthor:'User156',
-    messageDate:'',
-    authorList:example(),
-    eventList:getEventList()
+    messageAuthor: 'User156',
+    messageDate: '',
+    authorList: queryUsers(),
+    eventList: getEventList()
 };
 
-function example(){
-    return ['Кошкин','Кудрявцев', 'Туголуков', 'User15', 'User156'];
+function queryUsers() {
+    fetch('/query_users')
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+ 
+            response.json().then(function (data) {
+                console.log('query Users => ');
+                console.log(data)
+            });
+        })
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+
+    return ['Кошкин', 'Кудрявцев', 'Туголуков', 'User15', 'User156'];
 }
 
-function getEventList(){
+sendData();
+
+function sendData() {
+    var sendData = {
+        1:'one',
+        2:'two'
+    };
+
+    $;
+    fetch('/send_data', {
+            method: 'POST',
+            body: JSON.stringify(sendData),
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+        // .then(json)
+        .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+    });
+
+    // var ajaxSettings = {
+    //     method: 'post',
+    //     url: '/send_data',
+    //     data: JSON.stringify(sendData),
+    //     dataType: 'json',
+    //     contentType: 'application/json',
+    //     processData: false,
+    //     xhrFields: {
+    //         withCredentials: true
+    //     }
+    // };
+    // $.ajax(ajaxSettings);
+}
+
+function getEventList() {
     var list = [];
 
-    for(var i=0, len=localStorage.length; i<len; i++) {
+    for (var i = 0, len = localStorage.length; i < len; i++) {
         var key = localStorage.key(i),
             date, hours, min;
-            
+
         list.push(JSON.parse(localStorage[key]));
 
         date = new Date(list[i].eventDate);
-        hours = date.getHours().lenght == 1 ? '0'+date.getHours():date.getHours();
-        min = date.getMinutes().lenght == 1 ? '0'+date.getMinutes():date.getMinutes();
+        hours = date.getHours().lenght == 1 ? '0' + date.getHours() : date.getHours();
+        min = date.getMinutes().lenght == 1 ? '0' + date.getMinutes() : date.getMinutes();
         list[i].correctEventDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + ' ' + hours + ':' + min;
-        
+
         date = new Date(list[i].startDate);
-        hours = date.getHours().lenght == 1 ? '0'+date.getHours():date.getHours();
-        min = date.getMinutes().lenght == 1 ? '0'+date.getMinutes():date.getMinutes();
+        hours = date.getHours().lenght == 1 ? '0' + date.getHours() : date.getHours();
+        min = date.getMinutes().lenght == 1 ? '0' + date.getMinutes() : date.getMinutes();
         list[i].correctStartDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + ' ' + hours + ':' + min;
     }
-    
+
     return list;
 }
 
 export default function user(state = initialState, action) {
-    switch(action.type) {
+    switch (action.type) {
         case 'CHANGE_TEXT': {
             return { ...state, text: action.value }
         }
@@ -48,30 +106,30 @@ export default function user(state = initialState, action) {
         case 'CHANGE_LOCATION': {
             return { ...state, location: action.value }
         }
-        case 'CHANGE_EVENTDATE':{
+        case 'CHANGE_EVENTDATE': {
             var date = new Date(action.value);
             date.setDate(date.getDate() - 5);
-            if(date < new Date()) {
+            if (date < new Date()) {
                 date = new Date();
             }
-            
+
             return { ...state, eventDate: action.value, startDate: date }
         }
-        case 'CHANGE_STARTDATE':{
+        case 'CHANGE_STARTDATE': {
             return { ...state, startDate: action.value }
         }
-        case 'BUTTON_SAVE':{
+        case 'BUTTON_SAVE': {
             var lenght = localStorage.length;
-            localStorage.setItem( lenght+1, JSON.stringify({
-                                text : state.text.trim(),
-                                author: state.author,
-                                location: state.location.trim(),
-                                eventDate: state.eventDate,
-                                startDate: state.startDate,
-                                messageAuthor: state.messageAuthor,
-                                messageDate: new Date()
-                             }));
-            return { ...state, eventList:getEventList(), text: '', author: state.messageAuthor, location: '', eventDate: new Date(), startDate: new Date() }
+            localStorage.setItem(lenght + 1, JSON.stringify({
+                text: state.text.trim(),
+                author: state.author,
+                location: state.location.trim(),
+                eventDate: state.eventDate,
+                startDate: state.startDate,
+                messageAuthor: state.messageAuthor,
+                messageDate: new Date()
+            }));
+            return { ...state, eventList: getEventList(), text: '', author: state.messageAuthor, location: '', eventDate: new Date(), startDate: new Date() }
         }
     }
     return state;
