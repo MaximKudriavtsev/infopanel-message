959c816a8adcfada90a16e9be188543b7d6661e7
Maxim Kudryavtsev <maxim71ruswow@gmail.com>
2017-07-06 12:59:13 +0300

    added var tableData

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
 
+var tableData = require('./tableData.json');
+
 var app = new (require('express'))();
 var port = 3000;
 
