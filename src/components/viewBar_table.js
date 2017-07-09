2108782ce2cc4d84b337db480dfa2a6df2cb37e5
MK <maxim_71rus_wow@mail.ru>
2017-07-09 22:24:58 +0400

    added another viewBar with: - click function - hover - correct styles

---
+++ src/components/viewBar_table.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ReactDOM from 'react-dom'
+
+import Row from './Row.js'
+
+class ViewBar_table extends Component {
+    addScroll() {
+        var that = this,
+            elemTable = ReactDOM.findDOMNode(that.refs['table']),
+            isScroll = elemTable.offsetWidth > elemTable.scrollWidth,
+            elemHead = ReactDOM.findDOMNode(that.refs['viewBar_head']);
+
+        isScroll ? elemHead.setAttribute('class', 'viewBar_head') : elemHead.setAttribute('class', '');
+    }
+
+    componentDidMount() {
+        this.addScroll();
+    }
+    componentDidUpdate() {
+        this.addScroll();
+    }
+
+    render() {
+        var that = this,
+            template,
+            data = that.props.user.eventList,
+            clientHeight = document.documentElement.clientHeight,
+            height = clientHeight - 92;
+
+        if (data.length) {
+            template = data.map(function (item, index) {
+                return (
+                    <Row data={item} key={index} />
+                )
+            });
+        } else {
+            return (<div>No data for view...</div>)
+        }
+        return (
+            <div className='viewBar'>
+                <div className='' ref='viewBar_head'>
+                    <table className='viewBar_table'>
+                        <tbody>
+                            <tr className='viewBar_table_head'>
+                                <td className='viewBar_table_text'>Text</td>
+                                <td className='viewBar_table_author'>Author</td>
+                                <td className='viewBar_table_location'>Location</td>
+                                <td className='viewBar_table_eventDate'>Date of event</td>
+                                <td className='viewBar_table_startDate'>Publish date of event</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className='table' ref='table' style={{height: height}}>
+                    <table className='viewBar_table_'>
+                        <tbody className='viewBar_tbody'>
+                            {template}
+                        </tbody>
+                    </table>
+                </div>
+            </div >
+        )
+    }
+}
+function mapStateToProps(state) {
+    return {
+        user: state
+    };
+}
+export default connect(mapStateToProps, null)(ViewBar_table);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 import EditBar from './EditBar';
 import TitleBar from '../components/TitleBar';
-import ViewBar from '../components/ViewBar';
+import ViewBar_table from '../components/ViewBar_table';
 
 export default class App extends Component {
   render() {
     return <div className='App'>
         <TitleBar />
         <EditBar />
-        <ViewBar />
+        <ViewBar_table />
       </div>
   }
 }
