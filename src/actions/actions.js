240f3af18dff8592377dfa7ba8ea847145772d34
Maxim Kudryavtsev <maxim71ruswow@gmail.com>
2017-07-13 16:42:39 +0300

    Added union resolveActions and clientActions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,5 @@
 import { createActions } from 'resolve-redux';
 import recordAggregate from '../common/aggregates/record';
+import clientActions from './clientActions';
 
-export default createActions(recordAggregate);
+export default createActions(recordAggregate, clientActions);
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import actions from '../actions/actions';
-import actions_client from '../actions/actions_client';
 import EditBar from './EditBar';
 import TitleBar from '../components/TitleBar';
 import ViewBar_table from '../components/ViewBar_table';
@@ -11,25 +10,7 @@ import ViewBar_table from '../components/ViewBar_table';
 class App extends Component {
   render() {
     console.log(this.props.appActions);
-    //this.props.appActions.updateRecord('3', ' ');
-    // var tmp = {
-    //   record:{
 
-    //   aggregateId:'0',
-    //   id: -1,
-    //   text: '123',
-    //   author: 'Max',
-    //   location: '123',
-    //   eventDate: new Date(),
-    //   startDate: new Date(),
-    //   messageAuthor: 'Max',
-    //   messageDate: '123',
-    //   focusRow: '123'
-    //   }
-    // };
-    // this.props.appActions.addRecord('1', tmp);
-
-    //this.props.appActions.completeRecord('1', '1');
     return <div className='App'>
         <TitleBar />
         <EditBar />
