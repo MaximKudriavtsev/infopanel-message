9fc921df15d62fc4107acbe3bcac94b165ad16dc
CORP\Student15 <maxim71ruswow@gmail.com>
2017-07-17 09:42:20 +0300

    get users from mongoDB

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var io = new (require('socket.io'))(server);
 
 var port = 3000;
 
-//var userList = require('./userList.json');
 var userRecords = require('./userRecords.json');
 
 var compiler = webpack(config);
@@ -22,19 +21,8 @@ app.use(webpackHotMiddleware(compiler));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(bodyParser.json()); // for parsing application/json
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", req.headers.origin);
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 var MongoClient = require('mongodb').MongoClient,
   assert = require('assert');
-
-// Connection URL
-var uri = 'mongodb://InfoPanel:d221241M@noomid-shard-00-00-uso0b.mongodb.net:27017,noomid-shard-00-01-uso0b.mongodb.net:27017,noomid-shard-00-02-uso0b.mongodb.net:27017/events?ssl=true&replicaSet=NooMID-shard-0&authSource=admin';
 
 var event_store = require('./src/store/event_store');
 var subscribe = event_store.subscribe;
@@ -90,52 +78,39 @@ function getRecords(data) {
   }
   return recordList;
 }
-const findUsers = function (db, callback) {
-  return db.collection('userList')
-    .find({ corName: 'userList' })
-    .toArray();
-}
 
-function users() {
-  MongoClient.connect(config.storage.params.url, function (err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-
-    let userList = findUsers(db, db.close());
-
-    console.log(userList + ' after db');
-
-    console.log('db disconect');
-
-    return userList;
-  });
-}
 app.get('/', function (req, res) {
   fs.readFile('./index.html', 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     }
     executeQuery('records').then(state => {
-
-      let serverState = {
-        server: state,
-        client: {
-          id: '',
-          text: '',
-          author: 'Max',
-          location: '',
-          eventDate: new Date(),
-          startDate: new Date(),
-          messageAuthor: 'Max',
-          messageDate: '',
-          //authorList: getUsers(userList),
-          authorList: users(),
-          focusRow: ''
-        }
-      }
-
-      var result = data.replace(/{{PRELOADED_STATE}}/g, JSON.stringify(serverState));
-      res.send(result);
+      MongoClient.connect(config.storage.params.url, function (err, db) {
+        assert.equal(null, err);
+        db.collection('userList')
+          .find({ corName: 'userList' })
+          .map(function(item) { return {name: item.name, surname: item.surname}; } )
+          .toArray()
+          .then((users) => {
+            let serverState = {
+              server: state,
+              client: {
+                id: '',
+                text: '',
+                author: 'Max',
+                location: '',
+                eventDate: new Date(),
+                startDate: new Date(),
+                messageAuthor: 'Max',
+                messageDate: '',
+                authorList: users,
+                focusRow: ''
+              }
+            }
+            var result = data.replace(/{{PRELOADED_STATE}}/g, JSON.stringify(serverState));
+            res.send(result);
+          });
+      });
     }).catch(err => {
       console.log('Query error: ' + err.message);
       console.log(err);
@@ -164,43 +139,6 @@ app.post('/api/commands', function (req, res) {
     });
 });
 
-// app.get('/query_users', function (req, res) {
-//   //query from db
-//   res.send(userList);
-// });
-
-// app.get('/query_user_records', function (req, res) {
-//   //query from db
-//   res.send(userRecords);
-// });
-
-// app.post('/update_data', function (req, res, next) {
-//   res.send('=> /update_data');
-//   //post query to db
-//   console.log('=> /update_data');
-//   console.log(req.body);
-
-//   io.emit('action', { type: 'RECORD_DID_UPDATED' });
-// });
-
-// app.post('/create_data', function (req, res, next) {
-//   res.send('=> /create_data');
-//   //post query to db
-//   console.log('=> /create_data');
-//   console.log(req.body);
-
-//   io.emit('action', { type: 'RECORD_DID_CREATED' });
-// });
-
-// app.post('/delete_data', function (req, res, next) {
-//   res.send('=> /delete_data');
-//   //post query to db
-//   console.log('=> /delete_data');
-//   console.log(req.body);
-
-//   io.emit('action', { type: 'RECORD_DID_DELETED' });
-// });
-
 server.listen(port, function (error) {
   if (error) {
     console.error(error);
@@ -211,13 +149,6 @@ server.listen(port, function (error) {
 
 io.on('connection', function (socket) {
   console.log("Socket connected: " + socket.id);
-  //socket.emit('news', {data:'good day!'});
-  // io.on('action', function (action) {
-  //   if (action.type === 'server/hello') {
-  //     console.log('Got hello data!', action.data);
-  //     socket.emit('action', { type: 'message', data: 'good day!' });
-  //   }
-  // });
   const eventsNames = Object.keys(config.events).map(function (key) {
     return config.events[key];
   });
