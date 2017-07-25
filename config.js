233886bd8bf652044d4db4e142d561c905a47867
Maxim Kudryavtsev <maxim71ruswow@gmail.com>
2017-07-25 15:04:33 +0300

    Make config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  REDIRECT_HTTP: process.env.AUTH_REDIRECT_URL,
-  IP: process.env.HOST,
-  LOCAL_MONGO: process.env.MONGO_CONNECTION_URL,
-  USERS_COLLECTION: process.env.MONGO_COLLECTION_NAME,
+  REDIRECT_HTTP: process.env.AUTH_REDIRECT_URL || 'https://internal.devexpress.com/azure-auth',
+  IP: process.env.HOST || '127.0.0.1',
+  LOCAL_MONGO: process.env.MONGO_CONNECTION_URL || 'mongodb://mongodb-host:27017/Resolve',
+  USERS_COLLECTION: process.env.MONGO_COLLECTION_NAME || 'Events',
   APPLICATION_NAME: 'infopanel-message'
 }
